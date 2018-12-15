ec0b64e48f508b8181e7a82c25b17ae2bba657d6
Tomasz O <amstafik81@gmail.com>
2018-12-15 15:56:47 +0100

    konstruktor 2

--- src/_2_typy/OblugaSzkolen.java
+++ src/_2_typy/OblugaSzkolen.java
@@ -7,18 +7,8 @@ import java.time.Month;
 public class OblugaSzkolen {
     public static void main(String[] args) {
         Szkolenie szkolenieJava=new Szkolenie("Szkolenie Java",10,1000,LocalDate.of(2018, Month.DECEMBER,15));
+        System.out.println("\n\n");
         Szkolenie szkoleniePython=new Szkolenie("Szkolenie Python",10,1000,LocalDate.of(2018, Month.DECEMBER,15));
-        szkolenieJava.nazwa="Szkolenie Java";
-        szkolenieJava.cena=1000;
-        szkolenieJava.liczbaUczestnikow=10;
-        szkolenieJava.data= LocalDate.of(2018, Month.DECEMBER,15);
-
-
-        szkoleniePython.nazwa="Szkolenie Python";
-        szkoleniePython.cena=2000;
-        szkoleniePython.liczbaUczestnikow=20;
-        szkoleniePython.data=LocalDate.of(2018,Month.DECEMBER,15);
-
 
         System.out.println("Nazwa szkolenia:"+szkolenieJava.nazwa);
         System.out.printf("Cena szkolenia: %.2f PLN \n",szkolenieJava.cena);
