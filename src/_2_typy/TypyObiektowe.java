a5189af8d0787f38bff9e94bfd6c3670012e1eeb
Tomasz O <amstafik81@gmail.com>
2018-12-15 12:52:05 +0100

    pierwsze klasy

---
+++ src/_2_typy/TypyObiektowe.java
@@ -0,0 +1,40 @@
+package _2_typy;
+
+import java.util.Scanner;
+
+public class TypyObiektowe {
+    public static void main(String[] args) {
+        Scanner  wejscie=new Scanner(System.in);
+        System.out.print("Podaj swoje imie");
+        String imie=wejscie.nextLine().toLowerCase();
+
+        int ileznakow=imie.length();
+        String ostatnia=imie.toLowerCase().substring(imie.length()-1);
+        boolean czyKonczySieNaA=imie.toLowerCase().endsWith("a");
+        System.out.println(imie+" ma "+ileznakow+" znakow");
+
+        if (czyKonczySieNaA) {
+            if(imie.equalsIgnoreCase("Kuba")){
+                System.out.println("Imie meskie");
+
+            }
+            else {
+                System.out.println("Imie zenskie");
+            }
+        }
+        else {
+            System.out.println("Imie meskie");
+        }
+        if (czyKonczySieNaA && !imie.equalsIgnoreCase("Kuba")) {
+
+                System.out.println("Imie zenskie");
+            }
+        else {
+            System.out.println("Imie meskie");
+        }
+
+
+
+
+    }
+}
---
+++ src/_2_typy/TypyPrymitywne.java
@@ -0,0 +1,18 @@
+package _2_typy;
+
+public class TypyPrymitywne {
+    public static void main(String[] args) {
+        int iloscUczestnikow=8;
+        long duzaLiczba=4000000L;
+        byte malaLiczba=44;
+        short calkiemMalaLiczba=4;
+        System.out.println("Ilosc uczestnikow to:"+iloscUczestnikow);
+        float temperatura =0.5f;
+        double pi=3.14;
+
+        boolean czyDzisjajJestSobota=true;
+        char plec='K';
+
+
+    }
+}
