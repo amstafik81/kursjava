de579ce6eb7ef3ac702456827ceba3cc37a0b881
Tomasz O <amstafik81@gmail.com>
2019-01-19 14:18:06 +0100

    metody statyczne

---
+++ src/_8_Funkcje/TestowanieFunkcji2.java
@@ -0,0 +1,10 @@
+package _8_Funkcje;
+
+import static _8_Funkcje.FunkcjeGeometryczne.*;
+
+public class TestowanieFunkcji2 {
+    public static void main(String[] args) {
+        System.out.println("Pole kwadadratu o boku 2 wynosi:"+ poleKwadratu(Double.valueOf(2)));
+        System.out.println("Pole koła o promieniu 2 wynosi"+ poleKola(Double.valueOf(2)));
+    }
+}
