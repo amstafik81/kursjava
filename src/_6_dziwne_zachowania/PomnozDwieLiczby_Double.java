60554acf503160f4addadbbd7bb24875b0ffb211
Tomasz O <amstafik81@gmail.com>
2019-01-19 09:38:51 +0100

    zajecia nr 2

--- src/_6_dziwne_zachowania/PomnozDwieLiczby_Double.java
+++ src/_6_dziwne_zachowania/PomnozDwieLiczby_Double.java
@@ -1,5 +1,16 @@
 package _6_dziwne_zachowania;
 
-public class PomnozDwieLiczby_Double {
+import java.util.Scanner;
 
+public class PomnozDwieLiczby_Double {
+    public static void main(String[] args) {
+        Scanner wejscie=new Scanner(System.in);
+        System.out.println("Podaj dwie liczby");
+        Double arg1=wejscie.nextDouble();
+        Double arg2=wejscie.nextDouble();
+        Double wynik=arg1*arg2;
+        System.out.println(wynik);
+
+
+    }
 }
