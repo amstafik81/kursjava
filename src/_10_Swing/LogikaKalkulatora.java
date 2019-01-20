a60fb21babd5543ea0fad7cff3fcdd996d3a5ddb
Tomasz O <amstafik81@gmail.com>
2019-01-20 16:34:29 +0100

    Aplikacja okienkowa_za_pomoca_etbeans

---
+++ src/_10_Swing/LogikaKalkulatora.java
@@ -0,0 +1,14 @@
+package _10_Swing;
+
+public class LogikaKalkulatora {
+    public static double oblicz(double arg1, double arg2, String operacja) {
+        switch(operacja) {
+            case "+": return arg1 + arg2;
+            case "-": return arg1 - arg2;
+            case "*": return arg1 * arg2;
+            case "/": return arg1 / arg2;
+            default:  throw new IllegalArgumentException("Nieznana operacja " + operacja);
+        }
+    }
+
+}
